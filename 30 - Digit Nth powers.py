a8ee3acfb4d12e11c2cb66ec3d5de7986aa6373c
Christopher Kitching <113296322+MacSwish@users.noreply.github.com>
2023-07-22 21:30:44 +0100

    started new problem

---
+++ 30 - Digit Nth powers.py
@@ -0,0 +1,12 @@
+
+def find_upper_bound(N):
+    """Find upper bound on the number that could be the sum of its digits,
+    each raised to the Nth power
+
+    Args:
+        N (int): power to raise digits to
+    """
+
+
+N = int(input())
+upper_bound = find_upper_bound(N)
