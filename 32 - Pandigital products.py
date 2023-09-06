a8499db27693616d6cd19ec7a1fe9d7ed4deb071
C-Kitching <113296322+C-Kitching@users.noreply.github.com>
2023-09-06 18:35:17 +0100

    new problem

--- 32 - Pandigital products.py
+++ 32 - Pandigital products.py
@@ -1,44 +1,48 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
 import itertools
-from itertools import permutations
 
-def contains_pandigital_prod(perm, N):
+def build_num(size, pos, digits):
+    num = 0
+    for i in range(1, size+1):
+        num *= 10
+        num += digits[pos]
+        pos += 1
+    return num, pos
 
-    # join the permutation into a string
-    num = ''.join(str(i) for i in perm)
+n = int(input())
+digits = list(range(1, n+1))
 
-    if num == "391867254":
-        x = 5
+perms = list(itertools.permutations(digits))
+pandigitals = set()
 
-    # loop over all slicings into 3 numbers
-    for i in range(1, N-1):
-        for j in range(i+1, N):
-            a = int(num[:i])
-            b = int(num[i:j])
-            c = int(num[j:])
+sum_pandigitals = 0
 
-            # if it is pandigital product
-            if(a*b == c):
-                return True
+# loop through lengths of each number
+for digits in perms:
+    for a in range(1, n):
+        for b in range(1, n-a):
             
-    return False
-
-
-# read input
-N = int(input())
-
-# form the root pandigital number
-num = list(range(1, N+1))
-
-# determine all permutations
-perms = list(permutations(num))
-
-count_pandigital_prods = 0
-
-# loop over all pandigitals
-for perm in perms:
-
-    # count the number which are panditial products
-    if contains_pandigital_prod(perm, N):
-        count_pandigital_prods += 1
-
-print(count_pandigital_prods)
+            # determine c length
+            c = n - a - b
+            
+            # the product cant have shorter length than either
+            if(c < a or c < b): continue
+            
+            # contains currently used position in digits
+            pos = 0
+            
+            # determine the numbers
+            a_num, pos = build_num(a, pos, digits)
+            b_num, pos = build_num(b, pos, digits)
+            c_num, pos = build_num(c, pos, digits)
+            
+            # if pandigital and not product seen before
+            if(a_num*b_num == c_num and c_num not in pandigitals):
+                sum_pandigitals += c_num
+                pandigitals.add(c_num)
+            
+print(sum_pandigitals)
+            
+        
+            
